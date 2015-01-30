c9a0eade762269be1493266605926b84344354b4
Olivier Barbeau <obarbeau@acs-ami.com>
2015-01-30 12:47:54 +0100

    Version 2.6.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject repl-tasks "2.5.0"
+(defproject repl-tasks "2.6.0-SNAPSHOT"
   :description (str "PROJECTS|REPL-TASKS"
                     "Call lein tasks (and some lein plugins)"
                     "directly from the REPL")
