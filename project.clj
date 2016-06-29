1c7707c9784475584da02eb7220ff567da9dc789
Olivier Barbeau <olivier.barbeau@gmail.com>
2016-06-29 14:07:05 +0200

    Version 3.3.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject repl-tasks "3.2.0"
+(defproject repl-tasks "3.3.0-SNAPSHOT"
   :description (str "PROJECTS|REPL-TASKS"
                     "Call lein tasks (and some lein plugins),"
                     "as well as utility functions"
